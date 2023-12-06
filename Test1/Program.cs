f1ed6e8e1a97db076cb5245f0c9f55797b31fd55
Feodor_Zen <fedormerser@mail.ru>
2023-12-07 01:13:13 +0300

    test 1

---
+++ Test1/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+Console.Write("Введите натуральное число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число меньшее чем N - M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+if (M > N)
+{
+    Console.Write("Неверно поставлены значения");
+}
+if (M < 0)
+{
+    M = 0;
+}
+if (N > 0)
+{
+while (M < N + 1)
+{
+Console.Write(M + " ");
+M += 1;
+}
+}
+else
+{
+    Console.Write("Натуральных чисел нет");
+}
